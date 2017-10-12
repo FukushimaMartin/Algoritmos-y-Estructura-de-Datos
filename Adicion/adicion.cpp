19740882b95ab742bd6cb002efa4395fc5179723
FukushimaMartin <fukushimafederico@hotmail.com>
2017-10-12 12:19:11 -0300

    Create adicion.cpp

---
+++ Adicion/adicion.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+/* TP 1: "Adicion"
+* Federico Martin Fukushima
+* 01-04-2017
+*/
+
+int main() {
+    int i, j, suma;
+    
+    std::cout<<"Ingrese dos numeros enteros para sumarlos\n";
+    std::cout<<"Ingrese el primer numero: ";
+    std::cin>>i;
+    std::cout<<"Ingrese el segundo numero: ";
+    std::cin>>j;
+    suma = i+j;
+    std::cout<<"La suma de ambos numeros es: "<<suma;
+    
+}
