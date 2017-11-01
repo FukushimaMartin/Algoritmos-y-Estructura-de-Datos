f829dfc7a1a5d43d140788ff90c0e440453803d5
FukushimaMartin <fukushimafederico@hotmail.com>
2017-11-01 20:30:11 -0300

    Update Queue.cpp

--- Stack-Queue-Enlazados/Queue.cpp
+++ Stack-Queue-Enlazados/Queue.cpp
@@ -6,15 +6,19 @@ void enQueue(Queue& t, int y){
 	Node *q = new Node;
 	q->value = y;
 	t.rear->next = q;
+	t.rear = q;
+	if (t.nivel == 0){
+		t.front = t.rear;
+		t.rear->next = nullptr;
+	}
 	++t.nivel;
+	delete q;
 }
 int deQueue(Queue& t){
 	if (t.nivel != 0){
-		Node *p = t.front;
-		int valor = p->value;
-		t.front->next;
+		int valor = t.front->value;
+		t.front = t.front->next;
 		--t.nivel;
-		delete p;
 		return valor;
 	}
 	std::cout<<"Queue vacio!\n";
